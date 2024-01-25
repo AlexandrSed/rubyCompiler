758ae43731ad23e67e40f643a5cff508ef48f2ba
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-01-25 12:35:28 +0300

    написал реализацию функции createExprFromString

--- ExprNode.cpp
+++ ExprNode.cpp
@@ -9,5 +9,20 @@ public:
         result->idNode = ++ExprNode::max_id;
 	    return result;
     }
-    
+    static ExprNode* ExprNode::createExprFromString(string& str){
+        ExprNode* result = new ExprNode;
+        result->type = ExprType.stringType;
+        result->strValue = str;
+        result->idNode = ++ExprNode::max_id;
+	    return result;
+    }
+    static ExprNode* ExprNode::createExprFromTrue();
+    static ExprNode* ExprNode::createExprFromFalse();
+    static ExprNode* ExprNode::createExprFromNil();
+    static ExprNode* ExprNode::createExprFromInt(int val);
+    static ExprNode* ExprNode::createExprFromFloat(float val);
+    static ExprNode* ExprNode::createExprFromBinOp(ExprType typeOp, ExprNode* left, ExprNode* right);
+    static ExprNode* ExprNode::createExprFromFuncCall(std::vector<ExprNode*>* params, string& id);
+    static ExprNode* ExprNode::createExprFromUnary(ExprType typeOp, ExprNode* operand);
+    static ExprNode* ExprNode::createExprFromTernary(ExprNode* left, ExprNode* center, ExprNode* right);
 };
