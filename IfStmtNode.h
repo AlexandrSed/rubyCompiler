11d912b88c3245cc5c1ca16425212727bd604cb4
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-01-26 22:29:57 +0300

    написал файл IfStmtNode.h

---
+++ IfStmtNode.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include "ExprNode.h"
+
+class IfStmtNode {
+public:
+    ExprNode* condition;
+    std::vector<StmtNode*>* trueBranch;
+    std::vector<StmtNode*>* falseBranch;
+    std::vector<ElsifNode*>* elsif;
+
+    static IfStmtNode* createIfStmt(ExprNode* condition, std::vector<StmtNode*>* trueBranch);
+    static IfStmtNode* createIfStmtWithElse(ExprNode* condition, std::vector<StmtNode*>* trueBranch, std::vector<StmtNode*>* falseBranch);
+    static IfStmtNode* createIfStmtWithElsif(ExprNode* condition, std::vector<StmtNode*>* trueBranch, std::vector<ElsifNode*>* elsif);
+    static IfStmtNode* createIfStmtWithElseAndElsif(ExprNode* condition,
+                                    std::vector<StmtNode*>* trueBranch, std::vector<StmtNode*>* falseBranch, std::vector<ElsifNode*>* elsif);
+};
