2811cd0bfbd7dc2d6e0cfec392a8358ef7af6e87
suusumi <wer113wer@yandex.ru>
2024-01-28 21:13:06 +0300

    Исправления в лексере + начало дерева

---
+++ tree/tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wer11 on 28.01.2024.
+//
+
+#include "tree.h"
---
+++ tree/tree.h
@@ -0,0 +1,14 @@
+//
+// Created by wer11 on 28.01.2024.
+//
+
+#ifndef COMPILERLAB_TREE_H
+#define COMPILERLAB_TREE_H
+
+
+class tree {
+
+};
+
+
+#endif //COMPILERLAB_TREE_H
