b62f119465b43934b980c4b5a6b7bb85a77559b7
suusumi <wer113wer@yandex.ru>
2024-01-29 00:38:14 +0300

    Микрофиксы в начале создания дерева

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -1,5 +1,60 @@
-//
-// Created by wer11 on 28.01.2024.
-//
 
 #include "tree.h"
+#include <vector>
+
+using namespace std;
+
+tree::tree() {
+
+}
+
+//TODO: тут должен быть список всех ProgramElementNode
+void tree::printTree(std::vector<ProgramElementNode*>* nodes) {
+    outfile = ofstream("tree.dot");
+    outfile << "digraph Tree {\n";
+
+    printProgramElementNodeTree();
+
+    outfile << "}\n";
+    outfile.close();
+}
+
+void tree::printExprNodeTree(ExprNode *node) {}
+
+void tree::printStmtNodeTree(StmtNode *node) {
+    if (node != nullptr) {
+       
+
+        if (node->exprStmt != nullptr) {
+            outfile << "stmt_" << node->exprStmt << " [label=\"" << node->exprStmt << "\"];\n";
+        }
+        if (node->ifStmt != nullptr) {
+            outfile << "stmt_" << node->ifStmt << " [label=\"" << node->ifStmt << "\"];\n";
+        }
+
+        if (node->whileStmt != nullptr) {
+            outfile << "stmt_" << node->whileStmt << " [label=\"" << node->whileStmt << "\"];\n";
+        }   
+
+        if (node->forStmt != nullptr) {
+            outfile << "stmt_" << node->forStmt << " [label=\"" << node->forStmt << "\"];\n";
+        }
+
+        if (node->caseStmt != nullptr) {
+            outfile << "stmt_" << node->caseStmt << " [label=\"" << node->caseStmt << "\"];\n";
+        }
+
+        if (node->unlessStmt != nullptr) {
+            outfile << "stmt_" << node->unlessStmt << " [label=\"" << node->unlessStmt << "\"];\n";
+        }
+
+        if (node->returnStmt != nullptr) {
+            outfile << "stmt_" << node->returnStmt << " [label=\"" << node->returnStmt << "\"];\n";
+        }
+
+        if (node->aliasStmt != nullptr) {
+            outfile << "stmt_" << node->aliasStmt << " [label=\"" << node->aliasStmt << "\"];\n";
+            printStmtNodeTree(node)
+        }
+    }
+}
--- tree/tree.h
+++ tree/tree.h
@@ -1,12 +1,54 @@
-//
-// Created by wer11 on 28.01.2024.
-//
-
 #ifndef COMPILERLAB_TREE_H
 #define COMPILERLAB_TREE_H
 
+#include <stdio.h>
+#include <string>
+#include <list>
+#include <fstream>
+#include "parses_classes/AliasStmt/AliasStmtNode.h"
+#include "parses_classes/CaseStmt/CaseStmtNode.h"
+#include "parses_classes/ClassBodyStmt/ClassBodyStmtNode.h"
+#include "parses_classes/ClassStmt/ClassStmtNode.h"
+#include "parses_classes/ElseifNode/ElsifNode.h"
+#include "parses_classes/ExprNode/ExprNode.h"
+#include "parses_classes/ForStmt/ForStmtNode.h"
+#include "parses_classes/IfStmt/IfStmtNode.h"
+#include "parses_classes/MethodStmtNode/MethodStmtNode.h"
+#include "parses_classes/ProgramElementNode/ProgramElementNode.h"
+#include "parses_classes/ReturnStmt/ReturnStmtNode.h"
+#include "parses_classes/StmtNode/StmtNode.h"
+#include "parses_classes/UnlessStmt/UnlessStmtNode.h"
+#include "parses_classes/WhenStmt/WhenStmtNode.h"
+#include "parses_classes/WhileStmt/WhileStmtNode.h"
+#include <vector>
+using namespace std;
 
 class tree {
+private:
+    ofstream outfile;
+public:
+
+
+    tree();
+
+    //TODO: проверить printTree и printProgramElementNodeTree
+    void printTree(std::vector<ProgramElementNode*>* nodes);
+    void prinAliasStmtTree(AliasStmtNode* node);
+    void printCaseStmtTree(CaseStmtNode* node);
+    void printClassBodyTree(ClassBodyStmtNode* node);
+    void printClassStmt(ClassStmtNode* node);
+    void printElseifNodeTree(ElsifNode* node);
+    void printExprNodeTree(Expr* node);
+    void printForStmtTree(ForStmtNode* node);
+    void printIfStmtTree(IfStmtNode* node);
+    void printMethodStmtNodeTree(MethodStmtNode* node);
+    void printProgramElementNodeTree(ProgramElementNode* node);
+    void printReturnStmtTree(ReturnStmtNode* node);
+    void printStmtNodeTree(StmtNode* node);
+    void printUnlessStmtTree(UnlessStmtNode* node);
+    void printWhenStmtTree(WhenStmtNode* node);
+    void printWhileStmtTree(WhileStmtNode* node);
+
 
 };
 
